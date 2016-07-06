f8eee0f3b55afac6fc88813343fed10b2ba07854
エリス <ellis.bryan@gmail.com>
2016-07-06 19:22:56 +0900

    Moved www content to src/public. Removed www/css content. Updated project info transpile config structure. Updated main.js to import Onsen UI css and fonts.

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Page, Button} from 'react-onsenui';
 
-// load Onsen UI library
+// Load Onsen UI library
 import 'onsenui';
+
+// Onsen UI Styling and Icons
+require('onsenui/stylus/blue-basic-theme.styl');
+require('onsenui/css/font_awesome/css/font-awesome.min.css');
+require('onsenui/css/ionicons/css/ionicons.min.css');
+require('onsenui/css/material-design-iconic-font/css/material-design-iconic-font.min.css');
 
 class App extends React.Component {
 
