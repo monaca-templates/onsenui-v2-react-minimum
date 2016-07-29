08aaf37a5c32fb6211aa4554597a9d38d03aa8f4
エリス <ellis.bryan@gmail.com>
2016-07-29 09:43:33 +0900

    Refactored for Transpiler

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Page, Button} from 'react-onsenui';
+import {Page, Button, Toolbar} from 'react-onsenui';
+import {notification} from 'onsenui';
 
 export default class App extends React.Component {
+  alertPopup() {
+    notification.alert('This is an Onsen UI alert notification test.');
+  }
+
+  renderToolbar() {
+    return (
+      <Toolbar>
+        <div className='center'>Onsen UI</div>
+      </Toolbar>
+    );
+  }
 
   render() {
     return (
-      <Page>
+      <Page renderToolbar={this.renderToolbar}>
         <div>Hello World!</div>
-        <Button>Onsen UI Button</Button>
+        <Button onClick={this.alertPopup}>Click Me!</Button>
       </Page>
     );
   }
